45568444dbf3d40830c83e49ef6e29871a4c3747
unknown <lhduc94>
2024-01-05 01:44:06 +0700

    push book

--- _push_book.bat
+++ _push_book.bat
@@ -1,5 +1,5 @@
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-@REM Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
 git add .
 git commit -m "push book"
